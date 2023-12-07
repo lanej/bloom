06d89a71daaa1e0e859fd9e404de644fbce8e4cf
Joshua Lane <joshlane@easypost.com>
2023-12-07 11:00:36 -0800

    feat: add delimiter support and remove boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,38 @@
 use clap::Parser;
-use std::io::BufRead;
+use std::{borrow::Cow, io::BufRead};
 
-/// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
     #[arg(short, long, default_value_t = 16 * 1024)]
     bitmap_bytes: usize,
 
-    /// Number of times to greet
     #[arg(short, long, default_value_t = 16 * 1024)]
     count: usize,
+
+    #[arg(short, long)]
+    delimiter: Option<String>,
+
+    #[arg(short, long)]
+    index: Option<usize>,
 }
 
 fn main() {
     let args = Args::parse();
     let mut bloom: bloomfilter::Bloom<str> = bloomfilter::Bloom::new(args.bitmap_bytes, args.count);
+
     std::io::stdin().lock().lines().for_each(|l| {
         let line = l.unwrap();
-        if !bloom.check_and_set(&line) {
+        let mut input = Cow::Borrowed(&line);
+        if let (Some(d), Some(i)) = (&args.delimiter, &args.index) {
+            input = Cow::Owned(
+                line.split(d)
+                    .nth(*i)
+                    .unwrap_or_else(|| panic!("'{}': has no {} value separated by {}", line, i, d))
+                    .to_owned(),
+            );
+        }
+        if !bloom.check_and_set(&input) {
             println!("{}", line)
         }
     });
